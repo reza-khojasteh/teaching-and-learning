101e76b4361ed2b74bace1e2ea52e8f387b51812
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2020-12-11 15:01:09 -0500

    Do test push from (another) test local repo to remote master repo

--- programming/languages/javascript/basic-algorithms/recursion/hanoi.js
+++ programming/languages/javascript/basic-algorithms/recursion/hanoi.js
@@ -1,14 +1,13 @@
 function hanoi(first, second, third, number) {
-    if (number <= 0) {
-        console.log("At least one disk is needed!");
-    }
-    else if (number === 1) {
-        console.log(`Move from ${first} to ${third}`);
-    } else {
-        hanoi(first, third, second, number - 1);
-        console.log(`Move from ${first} to ${third}`);
-        hanoi(second, first, third, number - 1);
-    }
+  if (number <= 0) {
+    console.log("At least one disk is needed!");
+  } else if (number === 1) {
+    console.log(`Move from ${first} to ${third}`);
+  } else {
+    hanoi(first, third, second, number - 1);
+    console.log(`Move from ${first} to ${third}`);
+    hanoi(second, first, third, number - 1);
+  }
 }
 
-hanoi('f', 's', 't', 3);
+hanoi("f", "s", "t", 3);
