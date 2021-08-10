2f63a902d66190ce74cd5a63abe1511a5b343c2b
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-08-10 11:17:18 -0400

    Add some comments to solution.js of fetch-api dir.

--- programming/languages/javascript/fundamentals/fetch-api/solution.js
+++ programming/languages/javascript/fundamentals/fetch-api/solution.js
@@ -1,3 +1,4 @@
+// Functions declared with the async keyword allow us to write asynchronous code in a synchronous manner. And together with the Fetch API, we can make asynchronous API requests.
 // The Fetch API provides an interface for fetching resources(including across the network).It will seem familiar to anyone who has used XMLHttpRequest, but the new API provides a more powerful and flexible feature set.
 // The Response interface of the Fetch API represents the response to a request. Response.json() takes a Response stream, reads it to completion, and returns a promise that resolves with the result of parsing the response body text as JSON. Note that despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
 
