e60d713046bbf75b9c9097cd4d1edfa98e3aea7f
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-10-29 12:04:48 -0400

    Add to comments in "Fibonacci.js" to clarify more!

--- programming/languages/javascript/basic-algorithms/Dynamic Programming/Memoization/Fibonacci.js
+++ programming/languages/javascript/basic-algorithms/Dynamic Programming/Memoization/Fibonacci.js
@@ -32,11 +32,12 @@ const fib2 = (n) => {
 
 //Testing....
 console.time("Execution Time");
-console.log(fib1(45), lookup["fib1"]); //1134903170 3672623805
+console.log(fib1(45), lookup["fib1"]); //1134903170 3672623805(no. of nodes in the call tree)
 console.timeEnd("Execution Time"); //Around 13 seconds on my device
 
 console.time("Execution Time");
-console.log(fib2(45), lookup["fib2"]); //1134903170 89
-console.timeEnd("Execution Time"); //Around 0.4 milliseconds on my device! Just note that it uses more space -- of O(n) [= number of entries added] * O(1) [= size of each key/value pair for Fibonacci] on lookup -- to do that but seems worth it!
+console.log(fib2(45), lookup["fib2"]); //1134903170 89(no. of nodes in the call tree)
+console.timeEnd("Execution Time"); //Around 0.4 milliseconds on my device!
+// Just note that it uses more space-- of O(n)[= number of entries added] * O(1)[= size of each key / value pair for Fibonacci] on lookup-- to do that but seems worth it!
 
 //Also, the size of call stack storing all these active recursive calls in O(n) in both cases. So, overall, adding memoization doesn't impact the space complexity in here -- O(n) + O(n) = O(n) -- [in some examples it does because either the number of the keys is not simply a "1 changing parameter," or the size of each value in each pair isn't just an integer and hence, not O(1)!].
