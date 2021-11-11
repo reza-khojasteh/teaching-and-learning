f4117d9b8e12a092b3461b4da9fefb70c6fa17a4
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-11-10 22:10:39 -0500

    Change file name 'finding-all-subsequences.js' to 'printing-all-subsequences.js' and adding a second solution to it

---
+++ programming/languages/javascript/basic-algorithms/recursion/printing-all-subsequences.js
@@ -0,0 +1,32 @@
+// Javascript code to print all possible subsequences for a given Array using recursion
+
+function printAllSubsequences(array, index, subsequence) {
+  // Print the subsequence when reaching a leaf in the call tree
+  if (index === array.length) {
+    // Condition to avoid printing empty subsequences
+    if (subsequence.length > 0) console.log(`[${subsequence}]`);
+  } else {
+    // Solution 1:
+    // 1-a: Add the element at the current index of the array to the beginning of the next-to-be-formed subsequences
+    subsequence.push(array[index]);
+    // 1-b: Print all subsequences which do include the element at the current index of the array
+    printAllSubsequences(array, index + 1, subsequence);
+    // 2-a: Remove the element at the current index of the array from the beginning of the next-to-be-formed subsequences
+    subsequence.pop();
+    // 2-b: And print all subsequences which don't include the element at the current index of the array
+    printAllSubsequences(array, index + 1, subsequence);
+
+    // OR Solution 2:
+    // // 1: Print all Subsequences including the element at current index
+    // printAllSubsequences(array, index + 1, subsequence.concat([array[index]]));
+
+    // // 2: Print all Subsequences excluding the element at current index
+    // printAllSubsequences(array, index + 1, subsequence);
+  }
+}
+
+// Testing
+const array = ["a", "b", "c"];
+const subsequence = new Array();
+
+printAllSubsequences(array, 0, subsequence);
