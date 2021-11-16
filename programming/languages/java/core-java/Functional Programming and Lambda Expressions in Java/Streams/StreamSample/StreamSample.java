899e5efd0177924410b656f567c157a22a6fe04d
Reza Khojasteh <reza-khojasteh@users.noreply.github.com>
2021-11-16 10:48:56 -0500

    Update StreamSample.java
    Update comments

--- programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Streams/StreamSample/StreamSample.java
+++ programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Streams/StreamSample/StreamSample.java
@@ -2,7 +2,7 @@ import java.util.stream.Stream;
 
 /**
 * The StreamSample program implements a simple application utilizing Stream interface that:
-* 1- simply returns a sequential ordered stream whose elements are the specified values - calling Stream.of("Hello", "Java"), 
+* 1- simply returns a sequential ordered stream whose elements are the specified values - by calling Stream.of("Hello", "Java"), 
 * 2- then returns a stream consisting of the results of applying the given function (passed by lambda: s -> s + " world!" in here) to the elements of this stream,
 * 3- and finally, performs an action (printing here) for each element of this stream (using method reference println of System.out)
 *
