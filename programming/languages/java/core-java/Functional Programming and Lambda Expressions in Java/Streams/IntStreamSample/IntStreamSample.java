9b5cf17796e41a2ac02981751caf2654d1d72912
Reza Khojasteh <reza-khojasteh@users.noreply.github.com>
2021-11-16 10:13:24 -0500

    Update IntStreamSample.java
    Add comments in the code

--- programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Streams/IntStreamSample/IntStreamSample.java
+++ programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Streams/IntStreamSample/IntStreamSample.java
@@ -1,5 +1,14 @@
 import java.util.stream.IntStream;
 
+/**
+* The IntStreamSample program implements a simple application utilizing IntStream interface that:
+* 1- simply returns a sequential ordered stream whose elements are the specified values - using of(1, 2, 3), 
+* 2- then returns a stream consisting of the elements of the currently formed stream that match the given - implemented with lambda: i -> i % 2 == 0, 
+* 3- and finally, performs an action (printing here) for each element of this stream.
+*
+* @author Reza Khojasteh
+* @version 1.0
+*/
 public class IntStreamSample {
     public static void main(String[] args) {
         IntStream.of(1, 2, 3).filter(i -> i % 2 == 0).forEach(System.out::println);
