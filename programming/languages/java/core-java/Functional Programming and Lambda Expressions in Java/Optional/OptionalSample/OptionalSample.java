0cd7eb20ef88b6a5a12461de9fe3ef6ce545ecf8
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-11-17 09:14:13 -0500

    Create OptionalSample.java and update its README file to demonstrate a sample on Java's Optional

---
+++ programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Optional/OptionalSample/OptionalSample.java
@@ -0,0 +1,27 @@
+import java.util.Optional;
+import java.util.Random;
+
+/**
+ * The OptionalSample program implements a simple application utilizing Optional that:
+ * 1- simply creates a random number between 0 and 999 and prints it, 
+ * 
+ * 2- returns an Optional with the specified present non-null value - by calling Optional.of(randomNumber), 
+ * 3- returns an optional Integer based on the lambda passed to filter - by calling filter(i -> i % 2 != 0), 
+ * 4- returns an optional String based on the lambda passed to map - by calling map(i -> "Your random number, " + i + ", is odd!"),  
+ * 5- and finally, if a value is present, performs the given action (printing using method reference println of System.out) with the value, 
+ *    otherwise performs the given action. (printing "Your random was even!")
+ *
+ * @author Reza Khojasteh
+ * @version 1.0
+ */
+public class OptionalSample {
+	public static void main(String[] args) {
+		int randomNumber = new Random().nextInt(1000);
+		System.out.printf("Your random number is %d\n", randomNumber);
+
+		Optional.of(randomNumber)
+		.filter(i -> i % 2 != 0)
+		.map(i -> "Your random number, " + i + ", is odd!")
+		.ifPresentOrElse(System.out::println, () -> System.out.println("Your random was even!"));
+	}
+}
--- programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Optional/README.md
+++ programming/languages/java/core-java/Functional Programming and Lambda Expressions in Java/Optional/README.md
@@ -2,4 +2,4 @@
 
 Optional is a container object **which may or may not contain a non-null value** and can be helpful _to avoid [NullPointerException](https://docs.oracle.com/javase/8/docs/api/java/lang/NullPointerException.html)s in Java_.
 
-We can use the method **ifPresent()** (as an example and among other methods) to check if a value is present and then continue in the _funcitonal_ way.
+We can use the method **ifPresentOrElse()** (as an example and among other methods) to check _if a value is present and we should perform the given action with the value_, or _otherwise, we should perform another given action_.
