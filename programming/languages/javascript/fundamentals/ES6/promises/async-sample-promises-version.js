8c937800b0ee1d4684ace6d67e5f4a25599f03ac
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-11-19 20:17:00 -0500

    Add 'async-sample-promises-version.js' to demonstrate an async sample in JS using promises, hence solving 'callback hell' problem in 'just callbacks' version

---
+++ programming/languages/javascript/fundamentals/ES6/promises/async-sample-promises-version.js
@@ -0,0 +1,32 @@
+// Version 2: using promises (compare with version 1 please :-)
+
+const asyncFunction = () => {
+  // The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+  const promise = new Promise((resolve, reject) =>
+    setTimeout(
+      () =>
+        // 'resolve' is called when the asynchronous task completes successfully and returns the results of the task as a value.
+        // 'reject' is called when the task fails, and returns the reason for failure, which is typically an error object (not used in here though ;-)
+        resolve(
+          "Printing after another another two seconds, inside the callback passed to 'then', and in an async way!"
+        ),
+      2000
+    )
+  );
+
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("Two seconds is up now! Let's call asyncFunction....");
+  asyncFunction()
+    // The then() method returns a Promise.
+    .then((text) => {
+      console.log(text);
+      return asyncFunction();
+    })
+    // hence, here, getting rid of callback hells (please compare with version 1 ;-)
+    .then((text) => console.log(text));
+}, 2000);
+
+console.log("Printing first, with no delay, and sync!");
