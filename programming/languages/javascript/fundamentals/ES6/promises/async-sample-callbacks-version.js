7d4bd2229a3fa92b4f9390bedd565be1d1299536
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-11-19 19:58:50 -0500

    Add 'async-sample-callbacks-version.js' to demonstrate an async sample in JS using just callbacks, and its eventual 'callback hell' problem

---
+++ programming/languages/javascript/fundamentals/ES6/promises/async-sample-callbacks-version.js
@@ -0,0 +1,22 @@
+// Version 1: using just callbacks (compare with version 2 please :-)
+
+const asyncFunction = (callback) =>
+  setTimeout(
+    () =>
+      callback(
+        "Printing after another another two seconds, inside the callback passed when calling 'asyncFunction', and in an async way!"
+      ),
+    2000
+  );
+
+setTimeout(() => {
+  console.log("Two seconds is up now! Let's call asyncFunction....");
+  asyncFunction((text) => {
+    console.log(text);
+
+    // Off to a (nested) callback hell!
+    asyncFunction((text) => console.log(text));
+  });
+}, 2000);
+
+console.log("Printing first, with no delay, and sync!");
