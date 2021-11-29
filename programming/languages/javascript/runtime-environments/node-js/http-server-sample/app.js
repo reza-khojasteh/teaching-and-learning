9b4f54e2852fc73abf93c79a79f229ea92500429
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-11-29 15:33:36 -0500

    Update 'app.js'

--- programming/languages/javascript/runtime-environments/node-js/http-server-sample/app.js
+++ programming/languages/javascript/runtime-environments/node-js/http-server-sample/app.js
@@ -1,9 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server is running at port 3000...");
+});
