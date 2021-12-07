8dc9962b5e27d4a1f0b2f7711d0c69fc5626459d
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-12-06 20:16:34 -0500

    Modify 'two-sum.js' and Add 'array.js'

---
+++ programming/languages/javascript/data-structures/implementations/array/array.js
@@ -0,0 +1,44 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    // this.data = {};
+  }
+  get(index) {
+    return this[index];
+  }
+  push(item) {
+    this[this.length] = item;
+    this.length++;
+    return this;
+  }
+  pop() {
+    const lastItem = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  deleteAtIndex(index) {
+    const item = this[index];
+    this.shiftItems(index);
+    return item;
+  }
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
+    }
+    console.log(this[this.length - 1]);
+    delete this[this.length - 1];
+    this.length--;
+  }
+}
+
+const myArray = new MyArray();
+myArray.push("hi");
+myArray.push("you");
+myArray.push("!");
+myArray.pop();
+myArray.deleteAtIndex(0);
+myArray.push("are");
+myArray.push("nice");
+myArray.shiftItems(0);
+console.dir(myArray);
--- programming/languages/javascript/data-structures/two-sum/two-sum.js
+++ programming/languages/javascript/data-structures/two-sum/two-sum.js
@@ -3,22 +3,59 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) => {
-  const lookup = {};
 
-  for (let i = 0; i < nums.length; i++) lookup[nums[i]] = i;
+// Version 1: Brute Force
+// const twoSum = (nums, target) => {
+//   for (let p1 = 0; p1 < nums.length - 1; p1++) {
+//     const numberToFind = target - nums[p1];
 
-  for (let p1 = 0; p1 < nums.length; p1++) {
-    let p2 = lookup[target - nums[p1]];
-    if (p2 !== undefined && p2 !== p1) {
-      return [p1, p2];
+//     for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+//       if (numberToFind === nums[p2]) {
+//         return [p1, p2];
+//       }
+//     }
+//   }
+
+//   return null;
+// };
+// T(n) = O(n ^ 2), S(n) = O(1)
+
+// Version 2: Using A HashMap (JavaScript Obj.!) And Traversing The Array Twice
+// const twoSum = (nums, target) => {
+//   const lookup = {};
+
+//   for (let i = 0; i < nums.length; i++) lookup[nums[i]] = i;
+
+//   for (let p1 = 0; p1 < nums.length; p1++) {
+//     let p2 = lookup[target - nums[p1]];
+
+//     if (p2 !== undefined && p2 !== p1) return [p1, p2];
+//   }
+
+//   return null;
+// };
+// T(n) = O(n), S(n) = O(n)
+
+// Vresion 3: Using A HashMap (JavaScript Obj.!) And Traversing The Array Just Once!
+const twoSum = function (nums, target) {
+  const numsMap = {};
+
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+
+    if (currentMapVal !== undefined) return [currentMapVal, p];
+    else {
+      const numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
     }
   }
 
   return null;
 };
+// T(n) = O(n), S(n) = O(n)
 
 // Testing...
+console.log(twoSum([1, 3, 7, 9, 2], 11));
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
