e122ffbb7c5e904a4c9dfc76051ac86f45c7ad97
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-12-17 21:36:21 -0500

    Modify 'backspace-string-compare.js'

--- programming/languages/javascript/basic-algorithms/misc/backspace-string-compare/backspace-string-compare.js
+++ programming/languages/javascript/basic-algorithms/misc/backspace-string-compare/backspace-string-compare.js
@@ -1,40 +1,89 @@
 /** Version 1: Brute Force */
-const buildFinalStringArray = (string) => {
-  const builtString = [];
+// const buildFinalStringArray = (string) => {
+//   const builtString = [];
 
-  for (let p = 0; p < string.length; p++) {
-    if (string[p] !== "#") {
-      builtString.push(string[p]);
-    } else {
-      builtString.pop();
-    }
-  }
+//   for (let p = 0; p < string.length; p++) {
+//     if (string[p] !== "#") {
+//       builtString.push(string[p]);
+//     } else {
+//       builtString.pop();
+//     }
+//   }
 
-  return builtString;
-};
+//   return builtString;
+// };
 
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// const compareBackspaceStrings = (s, t) => {
+//   const finalS = buildFinalStringArray(s);
+//   const finalT = buildFinalStringArray(t);
+
+//   if (finalS.length !== finalT.length) {
+//     return false;
+//   } else {
+//     for (let p = 0; p < finalS.length; p++) {
+//       if (finalS[p] !== finalT[p]) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true;
+// };
+// T(m, n) = O(m + n), S(m, n) = O(m + n)
+
+/** Version 2: A better solution... */
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
 const compareBackspaceStrings = (s, t) => {
-  const finalS = buildFinalStringArray(s);
-  const finalT = buildFinalStringArray(t);
+  let p1 = s.length - 1,
+    p2 = t.length - 1;
 
-  if (finalS.length !== finalT.length) {
-    return false;
-  } else {
-    for (let p = 0; p < finalS.length; p++) {
-      if (finalS[p] !== finalT[p]) {
-        return false;
+  while (p1 >= 0 || p2 >= 0) {
+    if (s[p1] === "#") {
+      let backCount = 2;
+
+      while (backCount > 0) {
+        p1--;
+        backCount--;
+
+        if (s[p1] === "#") {
+          backCount += 2;
+        }
       }
+    }
+
+    if (t[p2] === "#") {
+      let backCount = 2;
+
+      while (backCount > 0) {
+        p2--;
+        backCount--;
+
+        if (t[p2] === "#") {
+          backCount += 2;
+        }
+      }
+    }
+
+    if (s[p1] !== t[p2]) {
+      return false;
+    } else {
+      p1--;
+      p2--;
     }
   }
 
   return true;
 };
-// T(m, n) = O(m + n), S(m, n) = O(m + n)
+// T(m, n) = O(m + n), S(m, n) = O(1)
 
 // Testing...
 console.log(compareBackspaceStrings("az#z", "ab#z"));
