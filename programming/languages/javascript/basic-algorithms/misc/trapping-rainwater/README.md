841f154d8d49b6500aefa52b432d7fa2df14e741
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-12-22 14:23:15 -0500

    Add a folder to solve the problem of 'Trapping Rain Water' in JS

---
+++ programming/languages/javascript/basic-algorithms/misc/trapping-rainwater/README.md
@@ -0,0 +1,7 @@
+# [Trapping Rain Water](https://leetcode.com/problems/trapping-rain-water/)
+
+_Trapping Rain Water_ is a problem stated as:
+
+> Given n non-negative integers representing an elevation map where the width of each bar is 1, **compute how much water it can trap after raining**.
+
+_You might also like to have a look at/run it [here](https://replit.com/@reza-khojasteh/Trapping-Rain-Water#index.js)_.
--- programming/languages/javascript/basic-algorithms/misc/trapping-rainwater/trapping-rainwater.js
+++ programming/languages/javascript/basic-algorithms/misc/trapping-rainwater/trapping-rainwater.js
@@ -2,6 +2,73 @@
  * @param {number[]} height
  * @return {number}
  */
+
+/** Version 1: Brute Force */
+// const findTrappingRainwater = (height) => {
+//   let totalWater = 0;
+
+//   for (let p = 0; p < height.length; p++) {
+//     let leftP = p,
+//       rightP = p,
+//       maxLeft = 0,
+//       maxRight = 0;
+
+//     while (leftP >= 0) {
+//       maxLeft = Math.max(maxLeft, height[leftP]);
+//       leftP--;
+//     }
+
+//     while (rightP < height.length) {
+//       maxRight = Math.max(maxRight, height[rightP]);
+//       rightP++;
+//     }
+
+//     const currentWater = Math.min(maxLeft, maxRight) - height[p];
+
+//     if (currentWater >= 0) {
+//       totalWater += currentWater;
+//     }
+//   }
+
+//   return totalWater;
+// };
+// T(n) = O(n ^ 2), S(n) = O(1)
+
+/** Version 2: A better solution... */
+// const findTrappingRainwater = function (height) {
+//   let left = 0,
+//     right = height.length - 1,
+//     totalWater = 0,
+//     maxLeft = 0,
+//     maxRight = 0;
+
+//   while (left < right) {
+//     if (height[left] <= height[right]) {
+//       if (height[left] >= maxLeft) {
+//         maxLeft = height[left];
+//       } else {
+//         totalWater += maxLeft - height[left];
+//       }
+
+//       left++;
+//     } else {
+//       if (height[right] >= maxRight) {
+//         maxRight = height[right];
+//       } else {
+//         totalWater += maxRight - height[right];
+//       }
+
+//       right--;
+//     }
+//   }
+
+//   return totalWater;
+// };
+// T(n) = O(n), S(n) = O(1)
+
+// OR
+
+/** Another version 2: A better solution... */
 const findTrappingRainwater = (height) => {
   const maxHeightIndex = height.indexOf(Math.max(...height));
   if (maxHeightIndex === -1) return 0;
