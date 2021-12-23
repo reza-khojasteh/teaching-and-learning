fe46ef6d9ff74dd823f67540fa0fa4575260e609
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2021-12-22 21:41:48 -0500

    Add a folder to solve the problem of 'Longest Substring Without Repeating Characters' in JS

---
+++ programming/languages/javascript/basic-algorithms/misc/longest-substring-without-repeating-characters/README.md
@@ -0,0 +1,7 @@
+# [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+
+_ Longest Substring Without Repeating Characters_ is a problem stated as:
+
+> Given a string s, **find the length of the longest substring without repeating characters**.
+
+_You might also like to have a look at/run it [here](https://replit.com/@reza-khojasteh/Longest-Substring-Without-Repeating-Characters#index.js)_.
---
+++ programming/languages/javascript/basic-algorithms/misc/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,66 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/** Version 1: Brute Force */
+// const lengthOfLongestSubstring = (s) => {
+//   if (s.length <= 1) return s.length; // Even if removed, code works correctly!
+
+//   let longest = 0;
+
+//   for (let left = 0; left < s.length /*&& s.length - left > longest*/; left++) {
+//     // The snippet in the comment above makes it 'a bit' more efficient/intelligent
+//     const seenChars = {};
+//     let currentLength = 0;
+
+//     for (let right = left; right < s.length; right++) {
+//       const currentChar = s[right];
+
+//       if (!seenChars[currentChar]) {
+//         currentLength++;
+//         seenChars[currentChar] = true;
+//         longest = Math.max(longest, currentLength);
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+
+//   return longest;
+// };
+// T(n) = O(n ^ 2), S(n) = O(n)
+
+/** Version 2: A better solution... */
+const lengthOfLongestSubstring = (s) => {
+  if (s.length <= 1) return s.length;
+
+  const seen = {};
+  let left = 0,
+    longest = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+    const previouslySeenChar = seen[currentChar];
+
+    if (previouslySeenChar >= left) {
+      left = previouslySeenChar + 1;
+    }
+
+    seen[currentChar] = right;
+
+    longest = Math.max(longest, right - left + 1);
+  }
+
+  return longest;
+};
+// T(n) = O(n), S(n) = O(n)
+
+// Testing...
+console.log(lengthOfLongestSubstring("abccabbqwertyuiopxzghf"));
+console.log(lengthOfLongestSubstring("abccabb"));
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring("p"));
+console.log(lengthOfLongestSubstring(""));
