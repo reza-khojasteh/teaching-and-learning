f80bdd1c6790d65184c0b0e67fea028f9977a31c
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-01-04 11:00:44 -0500

    Modify comments in selectionSort.js

--- programming/languages/javascript/basic-algorithms/sorting/selection-sort/selectionSort.js
+++ programming/languages/javascript/basic-algorithms/sorting/selection-sort/selectionSort.js
@@ -1,29 +1,29 @@
 //Selection Sort: O(n^2)
 function selectionSort(array) {
-    //if case array was null or undefined!
-    // if(!array) {
-    //     console.log('no array was passed');
-    //     return;
-    // }
-    //else
-    for (let i = 0; i < array.length - 1; i++) {
-        let min = array[i];
-        let minIndex = i;
-        //main body/operation
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[j] < min) {
-                min = array[j];
-                minIndex = j;
-            }
-        }
-        swap(i, minIndex);
+  //in case array was null or undefined!
+  // if(!array) {
+  //     console.log('no array was passed');
+  //     return;
+  // }
+  //else
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = array[i];
+    let minIndex = i;
+    //main body/operation
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < min) {
+        min = array[j];
+        minIndex = j;
+      }
     }
-    //swaps two elements in the array
-    function swap(i, j) {
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+    swap(i, minIndex);
+  }
+  //swaps two elements in the array
+  function swap(i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 
 //Test Data
