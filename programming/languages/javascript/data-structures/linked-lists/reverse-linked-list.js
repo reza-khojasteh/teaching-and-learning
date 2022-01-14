aecfb1d85509b6883feac9992239212388e448ae
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-01-13 23:53:03 -0500

    Add a solution to reverse a linked list in JS

---
+++ programming/languages/javascript/data-structures/linked-lists/reverse-linked-list.js
@@ -0,0 +1,43 @@
+// A linked list node
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+// Generate the linked list
+const linkedList = [5, 4, 3, 2, 1].reduce(
+  (acc, val) => new ListNode(val, acc),
+  null
+);
+
+// Print the linked list
+const printList = (head) => {
+  if (!head) {
+    return;
+  }
+
+  console.log(head.val);
+  printList(head.next);
+};
+
+// Reverse the linked list
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
+
+// Testing....
+printList(linkedList);
+console.log("after reverse");
+printList(reverseList(linkedList));
