5bf7de39ed08d165fa9e2635f6dc8640ec4c0b2a
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-01 23:46:55 -0500

    Add dfs traversing of a graph in JS

---
+++ programming/languages/javascript/data-structures/graph/graph-traversing/dfs/dfs.js
@@ -0,0 +1,38 @@
+// DFS traversing of a graph
+const dfs = (vertex) => {
+  visited[`${vertex}`] = true;
+  order.push(vertex);
+
+  for (const neighbor of graph[vertex])
+    if (!visited[`${neighbor}`]) dfs(neighbor);
+};
+
+// Testing: constructing a sample (directed) graph and calling dfs....
+let n = 6; // Number of nodes in the sample graph
+let edges = [
+  [1, 0],
+  [0, 3],
+  [3, 1],
+  [1, 2],
+  [1, 4],
+  [2, 4],
+  [3, 5],
+  [4, 5],
+]; // Array of edges in the sample (directed) graph; as an example, [0, 3] means that there is an edge from vertex 0 to vertex 3
+const graph = []; // Adjacency list representing graph
+
+// Initializing the graph's adjacency list
+for (let i = 0; i < n; i++) {
+  graph[i] = [];
+}
+
+edges.forEach((pair) => {
+  graph[pair[0]].push(pair[1]);
+});
+
+const visited = {}; // Hash/Set of visited vertices
+const order = []; // List/Order of traversed vertices by dfs
+
+// Calling dfs and prinitng out the result
+dfs(0);
+console.log(order);
