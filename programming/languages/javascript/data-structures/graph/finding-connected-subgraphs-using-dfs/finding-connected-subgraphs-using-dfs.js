70cfc93e729475e3f0c675f25e946345f7de8da4
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-03 20:31:10 -0500

    Add 'finding-connected-subgraphs-using-dfs.js'

---
+++ programming/languages/javascript/data-structures/graph/finding-connected-subgraphs-using-dfs/finding-connected-subgraphs-using-dfs.js
@@ -0,0 +1,55 @@
+// Finding connected subgraphs using dfs traversing
+const dfsConnectedSubgraphsFinder = () => {
+  const connectedSubgraphs = []; // This will contain connected subgraphs
+
+  // DFS traversing of a graph
+  const dfs = (vertex) => {
+    visited[vertex] = true;
+    order.push(vertex);
+
+    for (const neighbor of graph[vertex]) if (!visited[neighbor]) dfs(neighbor);
+  };
+
+  // Constructing a sample (directed) graph and calling dfs....
+  let n = 11; // Number of nodes in the sample graph
+  let edges = [
+    [1, 0],
+    [0, 3],
+    [3, 1],
+    [1, 2],
+    [1, 4],
+    [2, 4],
+    [3, 5],
+    [4, 5],
+    [6, 9],
+    [9, 7],
+    [8, 10],
+  ]; // Array of edges in the sample (directed) graph; as an example, [0, 3] means that there is an edge from vertex 0 to vertex 3
+  const graph = []; // Adjacency list representing graph
+
+  // Initializing the graph's adjacency list
+  for (let i = 0; i < n; i++) {
+    graph[i] = [];
+  }
+
+  edges.forEach((pair) => {
+    graph[pair[0]].push(pair[1]);
+  });
+
+  const visited = {}; // Hash/Set of visited vertices
+  let order = []; // List/Order of traversed vertices by dfs
+
+  // Calling dfs until no unvisited nodes are left!
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      connectedSubgraphs.push(order);
+      order = [];
+    }
+  }
+
+  return connectedSubgraphs;
+};
+
+// Testing....
+console.log(dfsConnectedSubgraphsFinder());
