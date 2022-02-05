1166814cd78da18225b43c1b35d9ee2b25d6c1c9
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-04 19:16:22 -0500

    Modify 'app.js' in 'http-server-sample'

--- programming/languages/javascript/runtime-environments/node-js/http-server-sample/app.js
+++ programming/languages/javascript/runtime-environments/node-js/http-server-sample/app.js
@@ -7,6 +7,8 @@ console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
 
+// server.on("connection", (req) => console.log("A new connection is made!"));
+
 server.listen(3000, () => {
   console.log("Server is running at port 3000...");
 });
