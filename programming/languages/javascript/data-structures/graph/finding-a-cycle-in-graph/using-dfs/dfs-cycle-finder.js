e26214ced25f3806e28b16d79bb4369bae248e2f
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-10 10:24:08 -0500

    Modify 'dfs-cycle-finder.js'

--- programming/languages/javascript/data-structures/graph/finding-a-cycle-in-graph/using-dfs/dfs-cycle-finder.js
+++ programming/languages/javascript/data-structures/graph/finding-a-cycle-in-graph/using-dfs/dfs-cycle-finder.js
@@ -1,5 +1,19 @@
 // Finding a cycle in a graph using dfs traversing (by adding a hash/set named 'path' to the original dfs.) Note that the algorithm saves the order of traversed nodes by the time the cycle was found (in the array named 'order'.)
+
+// DFS traversing of a graph (assuming that vertices are numbered 0 to n - 1)
+/**
+ * @param {number} vertex
+ * @return {boolean}
+ */
 const dfsCycleFinder = (vertex) => {
+  if (!graph[vertex]) {
+    // Vertex is not on the graph!
+    console.log(
+      "This vertex is not on the graph! Please try with another vertex!"
+    );
+    return;
+  }
+
   visited[vertex] = true;
   order.push(vertex);
 
@@ -46,5 +60,12 @@ const path = {}; // Current path we are on (used to check whether there is a cyc
 
 // Calling dfsCycleFinder and prinitng out the result
 // Note that we assume that there are no isolated nodes, otherwise we should continue calling dfsCycleFinder (as opposed to calling it just once, as it is now,) until no unvisited nodes are left!
-console.log(dfsCycleFinder(0));
-console.log(order);
+console.log(
+  dfsCycleFinder(0)
+    ? "There is a cycle in this graph!"
+    : "There are no cycles in this graph!"
+);
+console.log(
+  "The vertices are visited in this order (in case of a cycle, the vertices visited by the time cycle was detected):",
+  order
+);
