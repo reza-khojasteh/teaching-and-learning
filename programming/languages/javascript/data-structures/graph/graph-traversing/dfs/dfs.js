7c5abe436522bd153e84a80296c872411d02796e
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-10 12:56:03 -0500

    Modify 'dfs.js'

--- programming/languages/javascript/data-structures/graph/graph-traversing/dfs/dfs.js
+++ programming/languages/javascript/data-structures/graph/graph-traversing/dfs/dfs.js
@@ -1,4 +1,4 @@
-// DFS traversing of a graph (assuming that vertices are numbered 0 to n - 1)
+// DFS traversing of a graph
 /**
  * @param {number} vertex
  * @return {array} order
@@ -19,7 +19,6 @@ const dfs = (vertex) => {
 };
 
 // Testing: constructing a sample (directed) graph and calling dfs....
-let n = 6; // Number of nodes in the sample graph
 let edges = [
   [1, 0],
   [0, 3],
@@ -30,21 +29,40 @@ let edges = [
   [3, 5],
   [4, 5],
 ]; // Array of edges in the sample (directed) graph; as an example, [0, 3] means that there is an edge from vertex 0 to vertex 3
-const graph = []; // Adjacency list representing graph
 
-// Initializing the graph's adjacency list
-for (let i = 0; i < n; i++) {
-  graph[i] = [];
+const graph = []; // Initializing adjacency list (to represent the graph)
+
+// Creating the graph's adjacency list in 3 steps:
+// 1- Determining the set of vertices in the graph
+const setOfVertices = {};
+for (const pair of edges) {
+  if (!setOfVertices[pair[0]]) {
+    setOfVertices[pair[0]] = true;
+  }
+
+  // There might also be nodes that don't have out-degrees
+  if (!setOfVertices[pair[1]]) {
+    setOfVertices[pair[1]] = true;
+  }
+}
+console.log(setOfVertices);
+// 2- Initializing the graph's adjacency list
+for (const key in setOfVertices) {
+  graph[key] = [];
 }
 
+// 3- Adding the adjacents to each vertex's list
 edges.forEach((pair) => {
   graph[pair[0]].push(pair[1]);
 });
 
+// Initializing the visited hash/set and the order array:
 const visited = {}; // Hash/Set of visited vertices
 const order = []; // List/Order of traversed vertices by dfs
 
-// Calling dfs and prinitng out the result
+// Calling dfs (starting from the first element in 'setOfVertices') and printing out the result
 // Note that we assume that there are no isolated nodes, otherwise we should continue calling dfsCycleFinder (as opposed to calling it just once, as it is now,) until no unvisited nodes are left!
-dfs(0);
+isNaN(Object.keys(setOfVertices)[0])
+  ? dfs(Object.keys(setOfVertices)[0])
+  : dfs(+Object.keys(setOfVertices)[0]);
 console.log("The vertices are visited in this order:", order);
