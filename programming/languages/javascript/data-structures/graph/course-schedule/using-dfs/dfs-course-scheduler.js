c08b46e432856e19549c58ba9acb0db5ad63e301
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-02-15 23:30:09 -0500

    Modify 'dfs-cycle-finder.js'

--- programming/languages/javascript/data-structures/graph/course-schedule/using-dfs/dfs-course-scheduler.js
+++ programming/languages/javascript/data-structures/graph/course-schedule/using-dfs/dfs-course-scheduler.js
@@ -1,84 +1,86 @@
-// Finding topological sort, in case there are not any cycles in a graph using dfs traversing (by adding a hash/set named 'path' to the original dfs.)
-/**
- * @param {number} n
- * @param {array} edges
- * @return {boolean}
- */
-const courseScheduler = (n, edges) => {
-  // In case this function returns true, we can count on the order of traversed nodes as the topological sort (in the array named 'order'.)
-
-  // DFS traversing of a graph (we are here assuming that vertices are numbered 0 to n - 1; if this is not the case, we could take the approach in 'dfs.java')
+(() => {
+  // Finding topological sort, in case there are not any cycles in a graph using dfs traversing (by adding a hash/set named 'path' to the original dfs.)
   /**
-   * @param {number} vertex
-   * @return {array} order
+   * @param {number} n
+   * @param {array} edges
+   * @return {boolean}
    */
-  const dfsCourseScheduler = (vertex) => {
-    if (!graph[vertex]) {
-      // Vertex is not on the graph!
-      console.log(
-        "This vertex is not on the graph! Please try with another vertex!"
-      );
-      return;
+  const courseScheduler = (n, edges) => {
+    // In case this function returns true, we can count on the order of traversed nodes as the topological sort (in the array named 'order'.)
+
+    // DFS traversing of a graph (we are here assuming that vertices are numbered 0 to n - 1; if this is not the case, we could take the approach in 'dfs.java')
+    /**
+     * @param {number} vertex
+     * @return {array} order
+     */
+    const dfsCourseScheduler = (vertex) => {
+      if (!graph[vertex]) {
+        // Vertex is not on the graph!
+        console.log(
+          "This vertex is not on the graph! Please try with another vertex!"
+        );
+        return;
+      }
+
+      visited[vertex] = true;
+      order.push(vertex);
+
+      path[vertex] = true; // Add the current vertex to the current path
+
+      for (const neighbor of graph[vertex]) {
+        if (path[neighbor]) return false; // Return false if cycle detected in this call,
+
+        if (!visited[neighbor]) if (!dfsCourseScheduler(neighbor)) return false; // Or in one of the nested calls!
+      }
+
+      path[vertex] = undefined; // Remove the current vertex from the current path
+
+      return true;
+    };
+
+    const graph = []; // Adjacency list representing graph
+
+    // Initializing the graph's adjacency list
+    for (let i = 0; i < n; i++) {
+      graph[i] = [];
     }
 
-    visited[vertex] = true;
-    order.push(vertex);
+    edges.forEach((pair) => {
+      graph[pair[0]].push(pair[1]);
+    });
 
-    path[vertex] = true; // Add the current vertex to the current path
+    const visited = {}; // Hash/Set of visited vertices
+    const order = []; // List/Order of traversed vertices by dfsCourseScheduler (valid just if there are no detected cycles!)
+    const path = {}; // Current path we are on (used to check whether there is a cycle!)
 
-    for (const neighbor of graph[vertex]) {
-      if (path[neighbor]) return false; // Return false if cycle detected in this call,
-
-      if (!visited[neighbor]) if (!dfsCourseScheduler(neighbor)) return false; // Or in one of the nested calls!
-    }
-
-    path[vertex] = undefined; // Remove the current vertex from the current path
-
-    return true;
-  };
-
-  const graph = []; // Adjacency list representing graph
-
-  // Initializing the graph's adjacency list
-  for (let i = 0; i < n; i++) {
-    graph[i] = [];
-  }
-
-  edges.forEach((pair) => {
-    graph[pair[0]].push(pair[1]);
-  });
-
-  const visited = {}; // Hash/Set of visited vertices
-  const order = []; // List/Order of traversed vertices by dfsCourseScheduler (valid just if there are no detected cycles!)
-  const path = {}; // Current path we are on (used to check whether there is a cycle!)
-
-  // Calling dfsCourseScheduler until no unvisited nodes are left!
-  let cycle = false;
-  for (let i = 0; i < n && !cycle; i++) {
-    if (!visited[i]) {
-      if (!dfsCourseScheduler(i)) {
-        cycle = true;
+    // Calling dfsCourseScheduler until no unvisited nodes are left!
+    let cycle = false;
+    for (let i = 0; i < n && !cycle; i++) {
+      if (!visited[i]) {
+        if (!dfsCourseScheduler(i)) {
+          cycle = true;
+        }
       }
     }
-  }
 
-  if (!cycle) {
-    console.log("One possible order of taking these courses is:", order);
-  } else {
-    console.log("It won't be possible because of the presence of cycle(s).");
-  }
-};
+    if (!cycle) {
+      console.log("One possible order of taking these courses is:", order);
+    } else {
+      console.log("It won't be possible because of the presence of cycle(s).");
+    }
+  };
 
-// Testing: constructing a sample (directed) graph and calling dfsCourseScheduler....
-let n = 6; // Number of nodes in the sample graph
-let edges = [
-  // [1, 0], // Removing/commenting out this edge causes the cycle to be removed
-  [0, 3],
-  [3, 1],
-  [1, 2],
-  [1, 4],
-  [2, 4],
-  [3, 5],
-  [4, 5],
-]; // Array of edges in the sample (directed) graph; as an example, [0, 3] means that there is an edge from vertex 0 to vertex 3
-courseScheduler(n, edges);
+  // Testing: constructing a sample (directed) graph and calling dfsCourseScheduler....
+  let n = 6; // Number of nodes in the sample graph
+  let edges = [
+    // [1, 0], // Removing/commenting out this edge causes the cycle to be removed
+    [0, 3],
+    [3, 1],
+    [1, 2],
+    [1, 4],
+    [2, 4],
+    [3, 5],
+    [4, 5],
+  ]; // Array of edges in the sample (directed) graph; as an example, [0, 3] means that there is an edge from vertex 0 to vertex 3
+  courseScheduler(n, edges);
+})();
