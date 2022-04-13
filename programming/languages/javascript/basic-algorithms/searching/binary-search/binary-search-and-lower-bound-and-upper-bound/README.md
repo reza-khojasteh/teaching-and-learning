877a5aef3f9ebf18c80efdaab3123aa0376b94c5
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-04-13 14:34:16 -0400

    Add 'binary-search-and-lower-bound-and-upper-bound.js'

---
+++ programming/languages/javascript/basic-algorithms/searching/binary-search/binary-search-and-lower-bound-and-upper-bound/README.md
@@ -0,0 +1,9 @@
+# Binary Search and Lower Bound and Upper Bound
+
+The lower and upper bound of a binary search are **the lowest and highest positions where a value could be inserted without breaking the ordering.**
+
+As opposed to a typical binary search algorithm, lower bound search algorithm looks for **the first index of an element (lower bound)** and upper bound search algorithm looks for **the index after last occurrence (upper bound)**.
+
+Lower and upper bound search functions are useful when _trying to insert an element while maintaining a sorted order_, or when _searching for the number of a particular element in a sorted list_.
+
+_The example here, demonstrates these three_.
---
+++ programming/languages/javascript/basic-algorithms/searching/binary-search/binary-search-and-lower-bound-and-upper-bound/binary-search-and-lower-bound-and-upper-bound.js
@@ -0,0 +1,57 @@
+// The order of all the following algorithms are O(n)
+// Also, the last two could be combined by passing in a function to do the comparison (the rest are the same!)
+const binarySearch = (x, list) => {
+  let low = 0;
+  let high = list.length - 1;
+  let mid;
+
+  while (low < high) {
+    mid = Math.floor((low + high) / 2);
+
+    if (x === list[mid]) return mid;
+    else if (x < list[mid]) high = mid - 1;
+    else low = mid + 1;
+  }
+};
+
+const lowerBoundSearch = (x, list) => {
+  let low = 0;
+  let high = list.length - 1;
+  let mid;
+
+  while (low < high) {
+    mid = Math.floor((low + high) / 2);
+
+    if (x <= list[mid]) high = mid;
+    else low = mid + 1;
+  }
+
+  return low; //or return high;
+};
+
+const upperBoundSearch = (x, list) => {
+  let low = 0;
+  let high = list.length - 1;
+  let mid;
+
+  while (low < high) {
+    mid = Math.floor((low + high) / 2);
+
+    if (x < list[mid]) high = mid;
+    else low = mid + 1;
+  }
+
+  return low; //or return high;
+};
+
+// Testing...
+const nums = [1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 8, 9];
+console.log(binarySearch(3, nums)); // returns 7 in this example which is neither the first nor the last!
+//Even worse, if we add some elements to or remove some from the end of the array, the result would be different, each time we run(which is unreliable!)
+
+console.log(lowerBoundSearch(3, nums)); // returns 5 which is the actual first index of '3' in this example.
+console.log(upperBoundSearch(3, nums)); // returns 9 which is one more than the actual last index of '3' in this example.
+// Hence, to get the number of a specific element in the sorted array, we can do
+console.log(upperBoundSearch(3, nums) - lowerBoundSearch(3, nums)); // returns 4 in this example
+// And note that for a non-existent element, such as 7 in this case:
+console.log(upperBoundSearch(7, nums) - lowerBoundSearch(7, nums)); // returns 0 in this example
