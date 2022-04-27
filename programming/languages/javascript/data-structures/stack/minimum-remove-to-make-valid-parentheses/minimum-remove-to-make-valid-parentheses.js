5813da4ab5cfcdc38403f44be0822b76238a2244
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-04-27 11:53:14 -0400

    Add another solution for 'minimum-remove-to-make-valid-parentheses' in JS

--- programming/languages/javascript/data-structures/stack/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ programming/languages/javascript/data-structures/stack/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -1,34 +1,59 @@
+// const minimumRemoveToMakeValidParentheses = (s) => {
+//   let numOfParantheses = 0;
+//   const stack = [];
+//   let result = "";
+
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     let currentChar = s.charAt(i);
+
+//     if (currentChar === "(" && numOfParantheses - 1 >= 0) {
+//       stack.push(currentChar);
+//       numOfParantheses--;
+//     } else if (currentChar === ")") {
+//       stack.push(currentChar);
+//       numOfParantheses++;
+//     } else if (currentChar !== "(") {
+//       // It is a lower-case!
+//       stack.push(currentChar);
+//     }
+//   }
+
+//   while (stack.length != 0) {
+//     currentChar = stack.pop();
+
+//     if (numOfParantheses === 0) result += currentChar;
+//     else {
+//       if (currentChar === ")") numOfParantheses--;
+//       else result += currentChar;
+//     }
+//   }
+
+//   return result;
+// };
+// T(n) = O(n) and S(n) = O(n)
+
+// OR (another solution)
+
 const minimumRemoveToMakeValidParentheses = (s) => {
-  let numOfParantheses = 0;
+  const res = s.split("");
   const stack = [];
-  let result = "";
 
-  for (let i = s.length - 1; i >= 0; i--) {
-    let currentChar = s.charAt(i);
-
-    if (currentChar === "(" && numOfParantheses - 1 >= 0) {
-      stack.push(currentChar);
-      numOfParantheses--;
-    } else if (currentChar === ")") {
-      stack.push(currentChar);
-      numOfParantheses++;
-    } else if (currentChar !== "(") {
-      // It is a lower-case!
-      stack.push(currentChar);
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === "(") {
+      stack.push(i);
+    } else if (res[i] === ")" && stack.length) {
+      stack.pop();
+    } else if (res[i] === ")") {
+      res[i] = "";
     }
   }
 
-  while (stack.length != 0) {
-    currentChar = stack.pop();
-
-    if (numOfParantheses === 0) result += currentChar;
-    else {
-      if (currentChar === ")") numOfParantheses--;
-      else result += currentChar;
-    }
+  while (stack.length) {
+    const curIdx = stack.pop();
+    res[curIdx] = "";
   }
 
-  return result;
+  return res.join("");
 };
 // T(n) = O(n) and S(n) = O(n)
 
