499e92df20ca7ec3553753de7c3772a90bad9ce1
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-04-28 10:35:32 -0400

    Add a folder to show how to use (basic) pug/jade in a nodejs/express  app

---
+++ programming/languages/javascript/runtime-environments/node-js/using-pug-in-nodejs/routes/shop.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+});
+
+module.exports = router;
