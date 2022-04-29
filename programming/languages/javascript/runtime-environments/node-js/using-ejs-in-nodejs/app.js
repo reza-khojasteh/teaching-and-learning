7f0219464963b107fa8ee945930ec9c5d73f9e11
reza-Khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-04-29 16:04:42 -0400

    Add a folder to show the basics of how we can use ejs in nodejs

---
+++ programming/languages/javascript/runtime-environments/node-js/using-ejs-in-nodejs/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
+});
+
+app.listen(3000, () => console.log("Server is running at port 3000..."));
