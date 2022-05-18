9473f65ce84e98556f2d7d33aee89b7598e3c16f
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-05-18 09:17:11 -0400

    Add another solution to 'quickSort.js'

--- programming/languages/javascript/basic-algorithms/sorting/quick-sort/quickSort.js
+++ programming/languages/javascript/basic-algorithms/sorting/quick-sort/quickSort.js
@@ -5,51 +5,80 @@ const swap = (array, i, j) => {
   array[j] = temp;
 };
 
-// Definition of a function, partition, which partitions an array, based on the correct position of a pivot (which is set to the array's 'begin' at the beginning.)
-const partition = (array, begin, end) => {
-  let pivot = begin;
-  let left = begin + 1;
-  let right = end - 1;
+// First solution:
 
-  while (left < right) {
-    while (array[left] <= array[pivot] && left < end - 1) {
-      left++;
-    }
+// // Definition of a function, partition, which partitions an array, based on the correct position of a pivot (which is set to the array's 'begin' at the beginning.)
+// const partition = (array, begin, end) => {
+//   let pivot = begin;
+//   let left = begin + 1;
+//   let right = end - 1;
 
-    while (array[right] >= array[pivot] && right > begin) {
-      right--;
-    }
+//   while (left < right) {
+//     while (array[left] <= array[pivot] && left < end - 1) {
+//       left++;
+//     }
 
-    if (left < right) {
-      swap(array, left, right);
+//     while (array[right] >= array[pivot] && right > begin) {
+//       right--;
+//     }
+
+//     if (left < right) {
+//       swap(array, left, right);
+//     }
+//   }
+
+//   swap(array, pivot, right);
+//   return right;
+// };
+
+// // QuickSort:
+// const quickSort = (array, begin, end) => {
+//   // If array has no elements or has just one element, it's already sorted!
+//   if (end <= begin + 1) {
+//     return;
+//   }
+
+//   // If array has exactly two elements, sort it (if needed) and return!
+//   if (end === begin + 2) {
+//     if (array[begin] > array[end - 1]) {
+//       swap(array, begin, end - 1);
+//     }
+//     return;
+//   }
+
+//   // Partition the array:
+//   let right = partition(array, begin, end);
+
+//   // And recursively sort both partiotions
+//   quickSort(array, begin, right);
+//   quickSort(array, right + 1, end);
+// };
+
+// OR a second (simpler) solution:
+
+// Definition of a function, getPartition, which partitions an array, based on the correct position of a pivot (which is set to the array's 'last element' at the beginning.)
+const getPartition = (array, left, right) => {
+  const pivotElement = array[right];
+  let partitionIndex = left;
+
+  for (let j = left; j < right; j++) {
+    if (array[j] <= pivotElement) {
+      swap(array, partitionIndex, j);
+      partitionIndex++;
     }
   }
+  swap(array, partitionIndex, right);
 
-  swap(array, pivot, right);
-  return right;
+  return partitionIndex;
 };
 
 // QuickSort:
-const quickSort = (array, begin, end) => {
-  // If array has no elements or has just one element, it's already sorted!
-  if (end <= begin + 1) {
-    return;
+const quickSort = (array, left, right) => {
+  if (left < right - 1) {
+    const partitionIndex = getPartition(array, left, right - 1);
+    quickSort(array, left, partitionIndex);
+    quickSort(array, partitionIndex + 1, right);
   }
-
-  // If array has exactly two elements, sort it (if needed) and return!
-  if (end === begin + 2) {
-    if (array[begin] > array[end - 1]) {
-      swap(array, begin, end - 1);
-    }
-    return;
-  }
-
-  // Partition the array:
-  let right = partition(array, begin, end);
-
-  // And recursively sort both partiotions
-  quickSort(array, begin, right);
-  quickSort(array, right + 1, end);
 };
 
 // Testing...
