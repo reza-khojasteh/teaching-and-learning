761290057dd985250da59b9371b4dd5f23983d96
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-05-19 14:07:20 -0400

    Modify 'binarySearch.js'

--- programming/languages/javascript/basic-algorithms/searching/binary-search/binary-search-and-lower-bound-and-upper-bound/binary-search-and-lower-bound-and-upper-bound.js
+++ programming/languages/javascript/basic-algorithms/searching/binary-search/binary-search-and-lower-bound-and-upper-bound/binary-search-and-lower-bound-and-upper-bound.js
@@ -5,13 +5,15 @@ const binarySearch = (x, list) => {
   let high = list.length - 1;
   let mid;
 
-  while (low < high) {
+  while (low <= high) {
     mid = Math.floor((low + high) / 2);
 
     if (x === list[mid]) return mid;
     else if (x < list[mid]) high = mid - 1;
     else low = mid + 1;
   }
+
+  return -1; // We have not found that!
 };
 
 // The lowerBoundSearch based on BS above
@@ -46,7 +48,10 @@ const upperBoundSearch = (x, list) => {
 };
 
 // Testing...
-const nums = [1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 8, 9];
+let nums = [1, 2];
+console.log(binarySearch(3, nums));
+
+nums = [1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 8, 9];
 console.log(binarySearch(3, nums)); // returns 7 in this example which is neither the first nor the last!
 //Even worse, if we add some elements to or remove some from the end of the array, the result would be different, each time we run(which is unreliable!)
 
