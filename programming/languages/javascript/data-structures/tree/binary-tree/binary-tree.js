e4c660c4a65f4cfd0996e50cb9f180ed95b191a8
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-06-01 14:28:00 -0400

    Modify 'binary-tree.js'

--- programming/languages/javascript/data-structures/tree/binary-tree/binary-tree.js
+++ programming/languages/javascript/data-structures/tree/binary-tree/binary-tree.js
@@ -8,20 +8,19 @@ class BinaryTreeNode {
 }
 
 // Creating a binary tree from the data in an array, recursively
-const createBinaryTreeFromArrayR = (array = [], i = 0) => {
+// Note: the input array should have all the elements in all depths (even if null,) down to the last level that we still have (a) non-null value(s)!
+const createBinaryTreeFromArrayR = (array, i = 0) => {
   let root;
 
-  if (i >= array.length) {
-    root = null;
-    return null;
-  } else {
+  if (i >= array.length || array[i] === null) root = null;
+  else {
     root = new BinaryTreeNode(
       array[i],
       createBinaryTreeFromArrayR(array, 2 * i + 1),
       createBinaryTreeFromArrayR(array, 2 * i + 2)
     );
-    return root;
   }
+  return root;
 };
 
 // Testing....
@@ -31,5 +30,28 @@ console.log(root);
 root = createBinaryTreeFromArrayR([1, null, 2]);
 console.log(root);
 
-root = createBinaryTreeFromArrayR();
+root = createBinaryTreeFromArrayR([]);
+console.log(root);
+
+root = createBinaryTreeFromArrayR([
+  1,
+  1,
+  1,
+  1,
+  null,
+  null,
+  null,
+  1,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  1,
+]);
+console.log(root);
+
+root = createBinaryTreeFromArrayR([1, null, 2, null, null, null, 3]);
 console.log(root);
