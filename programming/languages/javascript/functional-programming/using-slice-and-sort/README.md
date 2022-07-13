5fc0b705f041e49933af6628ecca5a4d898407f5
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-07-13 16:49:32 -0400

    Add an example for 'using-slice-and-sort' in JS

---
+++ programming/languages/javascript/functional-programming/using-slice-and-sort/README.md
@@ -0,0 +1,3 @@
+# Uing Array's [slice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice) and [sort](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) methods in JS
+
+The example here, demonstrates how we can use _slice_ (in order to have a fresh copy of an array, so that we don't change the original one when applying functions like push, pop, reverse, sort, etc.) and _sort_ to **sort out the elemetns in an array (in a functional way!)**
---
+++ programming/languages/javascript/functional-programming/using-slice-and-sort/using-slice-and-sort.js
@@ -0,0 +1,29 @@
+// const sortedArray = array.slice.sort((a, b) => {
+//   //Assuming a and b are any two elements in the array:
+//   //if a should come before b, return a negative like -1
+//   //if b should come before a, return a positive like 1
+//   //if we should leave a and b in the order we found them, return 0
+// });
+
+// const ascending = (a, b) => {
+//   if (a < b) return -1;
+//   else if (a > b) return 1;
+//   else return 0;
+// };
+
+// const descending = (a, b) => {
+//   if (a < b) return 1;
+//   else if (a > b) return -1;
+//   else return 0;
+// };
+
+// console.log([2, 1, 3].slice().sort(ascending));
+// console.log([2, 1, 3].slice().sort(descending));
+
+// OR even simpler!
+const ascending = (a, b) => a - b;
+
+const descending = (a, b) => b - a;
+
+console.log([2, 1, 3].slice().sort(ascending));
+console.log([2, 1, 3].slice().sort(descending));
