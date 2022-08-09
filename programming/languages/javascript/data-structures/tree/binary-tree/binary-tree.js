0f5e81d74dac0b5f7e1023d4c9680027be7ebfcd
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-08-08 22:16:16 -0400

    Modify comments in 'binary-tree.js'

--- programming/languages/javascript/data-structures/tree/binary-tree/binary-tree.js
+++ programming/languages/javascript/data-structures/tree/binary-tree/binary-tree.js
@@ -13,7 +13,6 @@ class BinaryTreeNode {
   //    const root = new BinaryTreeNode(1);
   //    root.insert([1, 1, 1, null, null, null, 1, null, 1, null, null]);
   // create a BinaryTree which is equivalent of the second to last example below (and does it with fewer nodes/nulls!)
-
   /**
    * @param {array} array
    * @return {BinaryTreeNode} root
