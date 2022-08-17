34ede0b1ed8948367d29edec1dc64a6dbb290704
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-08-16 22:18:54 -0400

    Add first python program!

---
+++ programming/languages/python/core/days-to-hours.py
@@ -0,0 +1,30 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+
+
+def validate_and_execute():
+    try:
+        user_input_number = int(num_of_days_element)
+
+        # we want to do conversion only for positive integers
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
+        else:
+            print("you entered a negative number, no conversion for you!")
+    except ValueError:
+        print("your input is not a valid number. Don't ruin my program!")
+
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user, enter number of days as a comma separated list and I will convert it to hours!\n")
+    list_of_days = user_input.split(", ")
+    for num_of_days_element in set(list_of_days):
+        validate_and_execute()
