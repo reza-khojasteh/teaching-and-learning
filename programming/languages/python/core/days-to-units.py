8b690bd6344f5cda424fab58f40a05c80b485f99
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-09-04 16:44:09 -0400

    Modify 'days-to-units.py'

--- programming/languages/python/core/days-to-units.py
+++ programming/languages/python/core/days-to-units.py
@@ -1,30 +1,45 @@
-# A simple Python program to calculate the number of 'conversion_units' in a day...
-def days_to_units(num_ofs_days, conversion_unit):
+# A simple Python program to calculate the number of conversion_units pr day in a list composed of a few number of 'days:unit' elements...
+
+
+# A function to return the number of conversion_units(hours or minutes) in a given number of days passed in:
+def days_to_units(num_of_days, conversion_unit):
     if conversion_unit == "hours":
         return f"{num_of_days} days are {num_of_days * 24} hours"
     elif conversion_unit == "minutes":
         return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
     else:
-        return "unsupported unit"
+        return f"unsupported unit: '{conversion_unit}'!"
 
 
-def validate_and_execute():
+# A function to calculate the number of units in the number of days (values passed in as a dictionary):
+def calculate_units(days_and_unit_dictionary):
     try:
-        user_input_number = int(days_and_unit_dictionary["days"])
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
+        user_input_number_of_days = int(days_and_unit_dictionary["days"])
+        user_input_unit = days_and_unit_dictionary["unit"]
+
+        # we want to do conversion only for positive integers
+        if user_input_number_of_days > 0:
+            calculated_value = days_to_units(user_input_number_of_days, user_input_unit)
             print(calculated_value)
-        elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number")
+        elif user_input_number_of_days == 0:
+            print("you entered a 0! Please enter a valid positive number!")
         else:
-            print("you entered a negative number, no conversion for you!")
+            print(f"You entered a negative number: '{user_input_number_of_days}'! Please enter a valid positive number!")
+
     except ValueError:
-        print("your input is not a valid number. Don't ruin my program!")
+        print(f"Your input: '{days_and_unit_dictionary['days']}', is not a valid number!")
 
 
-user_input = ""
+# A loop to accept a list of 'days:unit' elements as the input from user and calculate units per day for each member of that list, until the user enters "exit"
+user_input = input("Enter a comma separated list of 'days:unit' elements and I will convert each element's 'days' to its equivalent 'unit's!\n")
 while user_input != "exit":
-    user_input = input("Hey user, enter number of days and conversion unit!\n")
-    days_and_unit = user_input.split(":")
-    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    validate_and_execute()
+    for key_value_entry in set(user_input.split(", ")):
+        try:
+            days_and_unit = key_value_entry.split(":")
+            days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+            calculate_units(days_and_unit_dictionary)
+        
+        except IndexError:
+            print("Error(s) found in the input format, please double check!")
+
+    user_input = input("Enter a comma separated list of 'days:unit' elements and I will convert each element's 'days' to its equivalent 'unit's!\n")
