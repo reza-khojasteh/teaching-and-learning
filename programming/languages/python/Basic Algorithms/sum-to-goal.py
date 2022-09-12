5323f848245d856983c9a4847cad3ad3a1445d5c
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-09-12 10:44:08 -0400

    Add 'sum-to-goal.py'

---
+++ programming/languages/python/Basic Algorithms/sum-to-goal.py
@@ -0,0 +1,13 @@
+def sum_to_goal(goal, list_of_numbers):
+    dictionary_of_numbers = {}
+
+    for i in list_of_numbers:
+        if dictionary_of_numbers.get(goal - i) is not None:
+            return i * dictionary_of_numbers[goal - i]
+        else:
+            dictionary_of_numbers[i] = i
+
+    return -1
+
+
+print(sum_to_goal(9, [1, 3, 5, 4, 5]))
