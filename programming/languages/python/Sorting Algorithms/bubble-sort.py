a1c9d8aca7f9937215fbba45c799fa350f2ab1aa
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-10-11 22:22:55 -0400

    Add 'bubble-sort.py'

--- programming/languages/python/Sorting Algorithms/bubble-sort.py
+++ programming/languages/python/Sorting Algorithms/bubble-sort.py
@@ -1,21 +1,47 @@
-#A simple program to sort a list of numbers using the bubble sort algorithm
-#O(n^2) solution    
+# A simple program to sort a list of numbers using the bubble sort algorithm with O(n^2)
+import random
+
+# Version 1
+
+
+# def bubble_sort(list):
+#     n = len(list)
+#     for i in range(n - 1):
+#         for j in range(n - 1):
+#             if list[j] > list[j + 1]:
+#                 list[j + 1], list[j] = list[j], list[j + 1]
+#                 # print(list)
+
+# Version 2
+
+
+# def bubble_sort(list):
+#     n = len(list)
+#     for i in range(n - 1):
+#         for j in range(n - 1 - i):
+#             if list[j] > list[j + 1]:
+#                 list[j + 1], list[j] = list[j], list[j + 1]
+#                 # print(list)
+
+# Version 3
+
+
 def bubble_sort(list):
     n = len(list)
+
     for i in range(n - 1):
-        for j in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
             if list[j] > list[j + 1]:
                 list[j + 1], list[j] = list[j], list[j + 1]
-                # print(list)
+                swapped = True
 
+        if not swapped:
+            break
 
-import random
 
 list = random.sample(range(1, 100), 10)
 print(f"Unsorted List: {list}")
 bubble_sort(list)
 print(f"Sorted List: {list}")
-
-
-
-
