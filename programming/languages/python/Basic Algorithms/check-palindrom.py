28bacbef42ea67506492f05948feb8b2f6024b24
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-10-17 11:12:01 -0400

    Add 'check-palindrom.py'

---
+++ programming/languages/python/Basic Algorithms/check-palindrom.py
@@ -0,0 +1,14 @@
+def is_palindrome(word):
+    def check_two_sides(left, right):
+        if left >= right:
+            return True
+        elif word[left] == word[right]:
+            return check_two_sides(left + 1, right - 1)
+        return False
+
+    return check_two_sides(0, len(word) - 1)
+
+
+# testing cases
+print(is_palindrome("noon"))
+print(is_palindrome("table"))
