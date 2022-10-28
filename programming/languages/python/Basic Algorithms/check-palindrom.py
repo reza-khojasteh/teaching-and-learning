a46b1e9bcf261d351802951cdcadc7dbc1c7848c
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2022-10-27 23:21:10 -0400

    Modify 'check-palindrom.py'

--- programming/languages/python/Basic Algorithms/check-palindrom.py
+++ programming/languages/python/Basic Algorithms/check-palindrom.py
@@ -1,12 +1,13 @@
 # A recursive solution to check if a string is a palindrome:
 def is_palindrome(word):
     def check_two_sides(left, right):
-        # Base case: if the left and right indices cross, we have a palindrome
+        # Base case: if the left and right indices cross, we have a palindrome,
         if left >= right:
             return True
         # General case: if the letters at the left and right indices are the same,
         elif word[left] == word[right]:
             return check_two_sides(left + 1, right - 1)
+        # General case: if the letters at the left and right indices are different,
         return False
 
     return check_two_sides(0, len(word) - 1)
