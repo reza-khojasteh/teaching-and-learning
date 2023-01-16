1ddc756292397be0d51df8504d7f99292668d45f
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-01-16 15:47:38 -0500

    Add an example to show how to read CSV files in Python

---
+++ programming/languages/python/core/csv_reader.py
@@ -0,0 +1,11 @@
+import csv
+import pprint
+
+with open("laureates.csv", "r") as f:
+    reader = csv.DictReader(f)
+    laureates = list(reader)
+
+for laureate in laureates:
+    if laureate["surname"] == "Einstein":
+        pprint.pprint(laureate)
+        break
