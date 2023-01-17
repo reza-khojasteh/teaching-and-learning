5ab9e32694ad13305afcbd3c9dc99be0e52a45b6
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-01-16 23:10:05 -0500

    Add two prints to using-datetime.py

--- programming/languages/python/core/using-datetime.py
+++ programming/languages/python/core/using-datetime.py
@@ -10,7 +10,9 @@ for laureate in laureates:
     if laureate["surname"] == "Einstein":
         # pprint(type(laureate)) # <class 'dict'>
         year_date = datetime.strptime(laureate["year"], "%Y")
+        # print(type(year_date))  # <class 'datetime.datetime'>
         born_date = datetime.strptime(laureate["born"], "%Y-%m-%d")
+        # print(type(born_date))  # <class 'datetime.datetime'>
         print(
             f"(Roughly) Age at which they received the prize: {year_date.year - born_date.year}")
         break
