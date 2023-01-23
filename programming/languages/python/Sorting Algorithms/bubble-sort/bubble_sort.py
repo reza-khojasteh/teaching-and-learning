3916bdcf87e43d79cfaa78e175ec01e8b576639a
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-01-23 14:33:12 -0500

    Add a folder for bubble sort and its tester in Py

--- programming/languages/python/Sorting Algorithms/bubble-sort/bubble_sort.py
+++ programming/languages/python/Sorting Algorithms/bubble-sort/bubble_sort.py
@@ -1,4 +1,7 @@
-# A simple program to sort a list of numbers using the bubble sort algorithm with O(n^2) for the worst case scenario
+# Author: Reza Khojasteh
+# Date: 2023-01-23
+# Description: A simple program to sort a list of numbers using the bubble sort algorithm with O(n^2) for the worst case scenario
+
 import random
 
 # Version 1
@@ -26,10 +29,30 @@ import random
 # Version 3 (with Ω(n) for the best case scenario)
 
 
+# def bubble_sort(list):
+#     n = len(list)
+
+#     for i in range(n - 1):
+#         swapped = False
+
+#         for j in range(n - 1 - i):
+#             if list[j] > list[j + 1]:
+#                 list[j + 1], list[j] = list[j], list[j + 1]
+#                 swapped = True
+
+#         if not swapped:
+#             break
+
+
+# Version 4 (the same as version 3, just in case you want to avoid using the 'break' statement)
+
+
 def bubble_sort(list):
     n = len(list)
+    i = 0
+    swapped = True
 
-    for i in range(n - 1):
+    while i < n - 1 and swapped:
         swapped = False
 
         for j in range(n - 1 - i):
@@ -37,8 +60,7 @@ def bubble_sort(list):
                 list[j + 1], list[j] = list[j], list[j + 1]
                 swapped = True
 
-        if not swapped:
-            break
+        i += 1
 
 
 # 'list' is a list of 10 randome numbers between 1 and 100, which can have duplicates
