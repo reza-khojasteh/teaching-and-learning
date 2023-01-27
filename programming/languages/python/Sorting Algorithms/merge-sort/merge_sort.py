13bb4b430a70681e478fe0bf626ae8bf16c0038d
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-01-27 15:08:37 -0500

    Modify 'merge_sort.py'

--- programming/languages/python/Sorting Algorithms/merge-sort/merge_sort.py
+++ programming/languages/python/Sorting Algorithms/merge-sort/merge_sort.py
@@ -1,5 +1,7 @@
+# Author: Reza Khojasteh
+# Date: 2023-01-27
 # Merge sort is a divide and conquer algorithm. It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is the key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one.
-# Merge sort requires O(n) extra space.
+# Merge sort requires O(nlog(n)) for the worst case and O(n) extra space.
 
 def merge_sort(my_list):
     n = len(my_list)
@@ -26,3 +28,7 @@ def merge_sort(my_list):
             my_list[k] = right[j]
             j += 1
             k += 1
+    # print(my_list) # uncomment this line to see the steps of the merge sort algorithm
+
+
+# merge_sort([4, 2, 3, 1]) # uncomment this line to see the steps of the merge sort algorithm
