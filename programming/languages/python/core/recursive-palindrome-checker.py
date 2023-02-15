60a0945cd205a02ae6cf4d5c3acb6a9f9660654b
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-02-15 14:50:57 -0500

    Modify 'recursive-palindrome-checker.py'

--- programming/languages/python/core/recursive-palindrome-checker.py
+++ programming/languages/python/core/recursive-palindrome-checker.py
@@ -12,4 +12,20 @@ def palindrome_checker(mystring):
     return recursive_palindrome_checker(mystring, 0, len(mystring) - 1)
 
 
-print(palindrome_checker("racacar"))
+print(palindrome_checker("racacare"))
+
+# Or:
+# def is_palindrome(word):
+#     def check_two_sides(left, right):
+#         if left >= right:
+#             return True
+#         elif word[left] == word[right]:
+#             return check_two_sides(left + 1, right - 1)
+#         return False
+
+#     return check_two_sides(0, len(word) - 1)
+
+
+# # testing cases
+# print(is_palindrome("noon"))
+# print(is_palindrome("table"))
