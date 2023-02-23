5d20cbd0401758deabe7b34722c6407f04949e12
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-02-23 15:15:09 -0500

    Modify 'recursive-palindrome-checker.py'

--- programming/languages/python/core/recursive-palindrome-checker.py
+++ programming/languages/python/core/recursive-palindrome-checker.py
@@ -1,31 +1,30 @@
-def recursive_palindrome_checker(mystring, a, b):
-    if a >= b:
-        return True
-    else:
-        if mystring[a] != mystring[b]:
-            return False
-        else:
-            return recursive_palindrome_checker(mystring, a + 1, b - 1)
+# A recursive solution to check if a string is a palindrome:
+def is_palindrome(word):
+    def check_two_sides(left, right):
+        # Base case: if the left and right indices cross, we have a palindrome,
+        if left >= right:
+            return True
+        # General case: if the letters at the left and right indices are the same,
+        elif word[left] == word[right]:
+            return check_two_sides(left + 1, right - 1)
+        # General case: if the letters at the left and right indices are different,
+        return False
+    # Call the recursive function with the left and right indices.
+    return check_two_sides(0, len(word) - 1)
 
 
-def palindrome_checker(mystring):
-    return recursive_palindrome_checker(mystring, 0, len(mystring) - 1)
+# testing cases
+print(is_palindrome("noon"))
+print(is_palindrome("table"))
+print(is_palindrome("racecar"))
+print(is_palindrome("tacocat"))
+print(is_palindrome(""))
+print(is_palindrome("a"))
 
-
-print(palindrome_checker("racacare"))
-
-# Or:
-# def is_palindrome(word):
-#     def check_two_sides(left, right):
-#         if left >= right:
-#             return True
-#         elif word[left] == word[right]:
-#             return check_two_sides(left + 1, right - 1)
-#         return False
-
-#     return check_two_sides(0, len(word) - 1)
-
-
-# # testing cases
-# print(is_palindrome("noon"))
-# print(is_palindrome("table"))
+# Output:
+# True
+# False
+# True
+# True
+# True
+# True
