d1538a99d2face16df77bdcb0effb314a2c63ea2
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2023-05-03 09:56:45 -0400

    Modify csv_reader.py

--- programming/languages/python/core/csv_reader.py
+++ programming/languages/python/core/csv_reader.py
@@ -1,12 +1,32 @@
+"""
+Reads data from a CSV file and prints the row for Albert Einstein.
+
+Parameters:
+- laureates.csv: The CSV file containing the data.
+
+Returns: None
+
+Functionality: 
+- Opens the CSV file "laureates.csv" for reading.
+- Creates a DictReader object to read the data into dictionaries.
+- Converts the DictReader into a list of dictionaries, one per row.
+- Loops through the rows, checking if the "surname" is "Einstein".
+- If a match is found, prints that row's dictionary.
+- Exits the loop after printing the match.
+"""
+
 import csv
 import pprint
 
-# open the file for reading
-with open("laureates.csv", "r") as f:
-    reader = csv.DictReader(f)
-    laureates = list(reader)
+try:
+    # open the file for reading
+    with open("laureates.csv", "r") as f:
+        reader = csv.DictReader(f)
+        laureates = list(reader)
 
-for laureate in laureates:
-    if laureate["surname"] == "Einstein":
-        pprint.pprint(laureate)
-        break
+    for laureate in laureates:
+        if laureate["surname"] == "Einstein":
+            pprint.pprint(laureate)
+            break
+except FileNotFoundError:
+    print("The file 'laureates.csv' does not exist!")
